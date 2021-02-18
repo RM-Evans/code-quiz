6a068106fe067947eb46fe021a6739c2bb50e51f
Michael Evans <Jawsbreaker@michaels-mbp.lan>
2021-02-17 18:16:02 -0700

    fixed timer and button to score page

--- app.js
+++ app.js
@@ -3,35 +3,37 @@
 window.addEventListener('load', onPageReady);
 
 // elements in the DOM
-let questionNumber, questionBody, questionOptions, sections, startQuiz, timer
+let questionNumber, questionBody, questionOptions, startQuiz, timer, saveScoreBtn
+
 
 // the state of our game
-const questions = [    {
-    question: "When should I use const?" ,
-    options: ["always", "when I dont want the variable to change", "when I want the variable to change"],
-    answer: "when I dont want the variable to change"
-},  
-{
-    question: "when I cant figure out how to debug something I ____.",
-    options: ["cry into my keyboard", "persevere and try to find my answer in docs", "all of the above"],
-    answer: "all of the above"
-},
-{
-    question: "Math.floor does what?",
-    options: ["returns the lowest number it can find", "returns the largest integer less than or equal to x", "rounds up a number"],
-    answer: "returns the largest integer less than or equal to x"
-},
-{
-    question: "what does a for loop do?",
-    options: ["loops through a block of code a number of times", "tElLS yOu WhAt FoR!", "the same as an if statement"], 
-    answer:"loops through a block of code a number of times"
-}
+let currentQuestionNumber = -1 // we have to start BEFORE our first question because we currentQuestionNumber++ when we start
+const questions = [    
+    {
+        question: "When should I use const?" ,
+        options: ["always", "when I dont want the variable to change", "when I want the variable to change"],
+        answer: "when I dont want the variable to change"
+    },  
+    {
+        question: "when I cant figure out how to debug something I ____.",
+        options: ["cry into my keyboard", "persevere and try to find my answer in docs", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        question: "Math.floor does what?",
+        options: ["returns the lowest number it can find", "returns the largest integer less than or equal to x", "rounds up a number"],
+        answer: "returns the largest integer less than or equal to x"
+    },
+    {
+        question: "what does a for loop do?",
+        options: ["loops through a block of code a number of times", "tElLS yOu WhAt FoR!", "the same as an if statement"], 
+        answer:"loops through a block of code a number of times"
+    }
 ];
-let currentQuestion = null
 let timeleft = null
+const scores = []
 
-
-
+// TODO: read from local storage; if exists, iterate and add each score
 
 
 // find all our elements on the page, and hook up the buttons we know about
@@ -40,12 +42,13 @@ function onPageReady() {
     questionBody = document.getElementById("questionBody")
     questionOptions = document.getElementById("questionOptions")
 
+    saveScoreBtn = document.getElementById("saveScore")
+    saveScoreBtn.addEventListener("click", onSaveScore)
+
     timer = document.getElementById("timer")
 
 
     startQuiz = document.getElementById("startQuiz")
-
-    sections = document.getElementsByTagName('section')
 
     startQuiz.addEventListener('click', onSetupQuiz)
     questionOptions.addEventListener('click', onOptionPress)
@@ -55,7 +58,7 @@ function onPageReady() {
 
 function onEverySecond() {
     //     // if there's no question, the quiz isn't running
-    if (currentQuestion === null) {
+    if( currentQuestionNumber < 0 || currentQuestionNumber >= questions.length ){
         return
     }
 
@@ -64,14 +67,16 @@ function onEverySecond() {
         currentQuestion = null
         showSection('review')
     } else {
-        timeleft = timeleft - 1
+        timeleft--;
         timer.innerHTML = timeleft
+        //print score in <h1>!!!
+        document.getElementById('printTimerResult').textContent = timer.innerHTML
     }
-
 
 
 };
 
+setInterval(onEverySecond, 1000)
 
 // setup our array of question
 function onSetupQuiz() {
@@ -86,7 +91,7 @@ function onSetupQuiz() {
     //     questions.push(q)
     // }
     timeleft = 60
-    setInterval(onEverySecond, 1000)
+    
     loadNextQuestion()
     showSection('question')
 }
@@ -94,21 +99,49 @@ function onSetupQuiz() {
 // get the next question 
 function loadNextQuestion() {
     // if there are questions left to answer, do the question
-    if (questions.length > 0) {
+    // if (questions.length > 0) { // since we no longer remove questions, this isn't valid
+
+
+    currentQuestionNumber++
+    if( currentQuestionNumber < questions.length ){
+
         // take the "first" question in the array
-        currentQuestion = questions.shift()
+        // currentQuestion = questions.shift() // don't delete them anymore!
+        // what array i want to use[is my iterator/place in array] = assign to variable to make life easier
+
+        const currentQuestion = questions[currentQuestionNumber]
 
         // how do we set question number?
+        //changing text inside the <p> with id of questionBody. namely our question
 
         questionBody.innerHTML = currentQuestion.question
 
-        let html = ""
-        for (let opt of currentQuestion.options) {
-            html = html + '<li><button value="' + opt + '">' + opt + "</button></li>"
+        // // OPTION a: use nasty strings
+        // let html = ""
+        // for (let opt of currentQuestion.options) {
+        //     html = html + '<li><button value="' + opt + '">' + opt + "</button></li>"
+        // }
+        // questionOptions.innerHTML = html
+
+
+        // OPTION 2, use the dom to add element objects
+
+        // first we have to empty out the <ul>
+        while(questionOptions.hasChildNodes()) { 
+            questionOptions.removeChild( questionOptions.firstChild ) 
         }
-        questionOptions.innerHTML = html
+        // now we create "objects" for each <li> and <button>
+        for(let opt of currentQuestion.options ){
+            const button = document.createElement("button")
+            button.value = opt 
+            button.textContent = opt 
+
+            const li = document.createElement("li")
+            li.appendChild(button)
+
+            questionOptions.appendChild(li)
+        }
     } else {
-        currentQuestion = null
         // calculate score? 
         showSection('review')
     }
@@ -123,18 +156,38 @@ function onOptionPress(e) {
     // if we get to here, they must have clicked a button!!!
 
 
+    const currentQuestion = questions[currentQuestionNumber]
 
     const correct = currentQuestion.answer === e.target.value
     if (correct) {
         loadNextQuestion()
     } else {
-        timeLeft = timeLeft - 5
-        alert('You chose poorly')
+        //remove time when you get it wrong and color button red
+        timeleft = timeleft - 5
+        //alert('You chose poorly')
+        e.target.style = "background: red"
     }
 }
 
+function onSaveScore() {
+    const initials = document.getElementById('initials').value
+    //alert(initials)
+    showSection("scores")
+    
+    // create some sort of array
+    scores.push({ 
+        initials: initials, 
+        score: timeleft 
+    })
+
+    // TODO: sort scores
+    // TODO: save `scores` to local storage
+    
+    // TODO: update DOM
+}
 
 function showSection(name) {
+    const  sections = document.getElementsByTagName('section')
     for (let section of sections) {
         if (section.id === name) {
             section.className = ''
